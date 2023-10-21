c0f1ef57ef0d71d41b516763241f2fdaa0ed2436
Szabolcs <info@szabidev.com>
2023-10-21 21:40:41 +0200

    basic settings

---
+++ config/options.lua
@@ -0,0 +1,13 @@
+vim.opt.number = true
+
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+
+vim.opt.shiftwidth = 4
+
+vim.opt.autoindent = true
+
+vim.opt.hlsearch = false
+
+vim.opt.incsearch = true
