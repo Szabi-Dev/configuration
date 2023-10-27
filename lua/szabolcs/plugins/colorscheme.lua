d0b5e3c64f2f785475198ffa5f8fa127a2540029
=szabolcs <info@szabidev.com>
2023-10-27 20:18:57 +0200

    nvim tree deleted mappings

--- lua/szabolcs/plugins/colorscheme.lua
+++ lua/szabolcs/plugins/colorscheme.lua
@@ -1,4 +1,4 @@
- return {
+return {
     "bluz71/vim-nightfly-guicolors",
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
--- lua/szabolcs/plugins/nvim-tree.lua
+++ lua/szabolcs/plugins/nvim-tree.lua
@@ -15,7 +15,7 @@ return {
     -- configure nvim-tree
     nvimtree.setup({
       view = {
-        width = 35,
+        width = 50,
         relativenumber = true,
       },
       actions = {
@@ -38,7 +38,5 @@ return {
 
     keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
     keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
-    keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
-    keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
   end,
 }
