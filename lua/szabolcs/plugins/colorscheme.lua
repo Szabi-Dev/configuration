1c183b3ad2af56ee9572eb6d66198f752897c724
Szabidev <info@szabidev.com>
2023-10-29 12:36:23 +0100

    changed colorscheme and transparent bg

--- lua/szabolcs/plugins/colorscheme.lua
+++ lua/szabolcs/plugins/colorscheme.lua
@@ -1,8 +1,8 @@
 return {
-    "bluz71/vim-nightfly-guicolors",
+    "sainnhe/everforest",
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- load the colorscheme here
-      vim.cmd([[colorscheme nightfly]])
-    end,
+      vim.cmd([[colorscheme everforest]])
+    end
   }
