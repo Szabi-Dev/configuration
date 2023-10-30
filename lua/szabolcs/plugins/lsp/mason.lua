f3c6fc684331c98e5a1078ca7e4c0d2fb78c6a62
szabolcs <szabolcs.csorgo@ciit.eu>
2023-10-30 12:52:11 +0100

    tsserver and html added

--- lua/szabolcs/plugins/lsp/mason.lua
+++ lua/szabolcs/plugins/lsp/mason.lua
@@ -25,6 +25,8 @@ return {
             -- list of servers for mason to install
             ensure_installed = {
                 "lua_ls",
+                "tsserver",
+                "html"
             },
             -- auto-install configured servers (with lspconfig)
             automatic_installation = true, -- not the same as ensure_installed
