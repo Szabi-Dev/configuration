46b0c74a90fbd721cfe218892b7ac54845f3ba51
szabolcs <szabolcs.csorgo@ciit.eu>
2023-11-02 20:09:25 +0100

    harpoon added

---
+++ lua/szabolcs/plugins/harpoon.lua
@@ -0,0 +1,16 @@
+return {
+  "ThePrimeagen/harpoon",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  config = function()
+    -- set keymaps
+    local keymap = vim.keymap -- for conciseness
+
+    keymap.set( "n", "<leader>hm", "<cmd>lua require('harpoon.mark').add_file()<cr>", { desc = "Mark file with harpoon" })
+    keymap.set( "n", "<leader>ht", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", { desc = "Toggle quick menu for harpoon" })
+    keymap.set("n", "<leader>hn", "<cmd>lua require('harpoon.ui').nav_next()<cr>", { desc = "Go to next harpoon mark" })
+    keymap.set( "n", "<leader>hp", "<cmd>lua require('harpoon.ui').nav_prev()<cr>", { desc = "Go to previous harpoon mark" }
+    )
+  end,
+}
