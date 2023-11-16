edcedf4db860c11f4696e8760d43651c41b34405
Szabidev <info@szabidev.com>
2023-11-16 14:27:44 +0100

    keymaps for navigation

---
+++ lua/szabolcs/core/keymaps.lua
@@ -0,0 +1,6 @@
+local keymap = vim.keymap;
+
+keymap.set('n', 'wh', '<C-w>h', { noremap = true, silent = true});
+keymap.set('n', 'wk', '<C-w>k', { noremap = true, silent = true});
+keymap.set('n', 'wj', '<C-w>j', { noremap = true, silent = true});
+keymap.set('n', 'wl', '<C-w>l', { noremap = true, silent = true});
