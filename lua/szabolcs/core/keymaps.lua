5f78818fec58f266cef95abab9c9f2dd9ae5a547
Szabolcs Csorgo <csorgosz@gmail.com>
2023-11-16 21:07:37 +0100

    navigation changed to leader key, select all added

--- lua/szabolcs/core/keymaps.lua
+++ lua/szabolcs/core/keymaps.lua
@@ -1,7 +1,10 @@
 local keymap = vim.keymap;
 
 -- Navigation between windows 
-keymap.set('n', 'wh', '<C-w>h', { noremap = true, silent = true});
-keymap.set('n', 'wk', '<C-w>k', { noremap = true, silent = true});
-keymap.set('n', 'wj', '<C-w>j', { noremap = true, silent = true});
-keymap.set('n', 'wl', '<C-w>l', { noremap = true, silent = true});
+keymap.set('n', '<leader>wh', '<C-w>h', { noremap = true, silent = true});
+keymap.set('n', '<leader>wk', '<C-w>k', { noremap = true, silent = true});
+keymap.set('n', '<leader>wj', '<C-w>j', { noremap = true, silent = true});
+keymap.set('n', '<leader>wl', '<C-w>l', { noremap = true, silent = true});
+
+-- Select all
+keymap.set('n', '<C-a>', 'ggVG', { noremap = true, silent = true});
