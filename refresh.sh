ba1c649e897f3190f8a891d1431cf45dd2bc9428
Szabidev <info@szabidev.com>
2023-11-19 15:56:55 +0100

    refresh script improved

--- refresh.sh
+++ refresh.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
 
 # refresh tmux configuration file
-rsync -P .tmux.conf ~/
+refresh_tmux() {
+    rsync -P .tmux.conf ~/
+}
+
+refresh_all() {
+    refresh_tmux
+}
+
+
+if [ $# -eq 0 ]; then
+    echo "No arguments provided, updating all the configs"
+    refresh_all
+    exit 1
+fi
+
+case $1 in
+
+    tmux)
+        refresh_tmux
+        ;;
+
+    *)
+        echo  "Unknown parameter"
+        ;;
+esac
